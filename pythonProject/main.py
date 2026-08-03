6c36c0534ac2895df244fafa984e62ce1c24973b
woshiyeshuhua-eng <woshiyeshuhua@gmail.com>
2026-08-03 15:50:00 +0800

    Update main.py

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -200,7 +200,7 @@ LEVELS = [
             "L1_M_2": "Notify a teacher",
             "L1_M_3": "Pinch the bridge of the nose",
             "L1_M_4": "Retrieve ice pack",
-            "L1_M_5": "Apply ice pack",
+            "L1_M_5": "Apply ice pack to bridge of the nose",
             "L1_M_6": "Check for any bleeding",
             
             # ============================================================
